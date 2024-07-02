28ebba9c2698f1de29f4aacd2c5e6cc939e1d212
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2024-07-03 05:16:50 +0530

    Update useScrollToRef.ts

--- client/src/hooks/useScrollToRef.ts
+++ client/src/hooks/useScrollToRef.ts
@@ -8,26 +8,14 @@ type TUseScrollToRef = {
 };
 
 export default function useScrollToRef({ targetRef, callback, smoothCallback }: TUseScrollToRef) {
- const logAndScroll = (
-  targetRef: React.RefObject<HTMLElement>, 
-  behavior: 'instant' | 'smooth', 
-  callbackFn: () => void
-) => {
+ const logAndScroll = (behavior: 'instant' | 'smooth', callbackFn: () => void) => {
   // Debugging:
   console.dir(targetRef.current); // This will log the detailed properties of the targetRef
   console.log(`Scrolling with behavior: ${behavior}, Time: ${new Date().toISOString()}`);
-
-  // Scroll to the bottom of the target element
-  if (targetRef.current) {
-    targetRef.current.scrollTo({
-      top: targetRef.current.scrollHeight,
-      behavior: behavior === 'smooth' ? 'smooth' : 'auto'
-    });
-  }
-
-  // Execute the callback function
+  // targetRef.current?.scrollIntoView({ behavior: ${behavior}, block: 'end' });
   callbackFn();
 };
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const scrollToRef = useCallback(
     throttle(() => logAndScroll('instant', callback), 250, { leading: true }),
