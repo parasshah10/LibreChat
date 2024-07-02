66ee5817ebec118961695f526a1743236b9ec868
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2024-07-03 04:05:23 +0530

    Update useScrollToRef.ts

--- client/src/hooks/useScrollToRef.ts
+++ client/src/hooks/useScrollToRef.ts
@@ -10,6 +10,7 @@ type TUseScrollToRef = {
 export default function useScrollToRef({ targetRef, callback, smoothCallback }: TUseScrollToRef) {
   const logAndScroll = (behavior: 'instant' | 'smooth', callbackFn: () => void) => {
     // Debugging:
+    console.log(`${targetRef}`);
     console.log(`Scrolling with behavior: ${behavior}, Time: ${new Date().toISOString()}`);
     targetRef.current?.scrollIntoView({ behavior });
     callbackFn();
