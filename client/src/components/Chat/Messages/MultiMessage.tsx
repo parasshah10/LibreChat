b59eee47237ebd16142d8db767c9fa46b8bab12e
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2024-07-05 02:47:34 +0530

    Update MultiMessage.tsx

--- client/src/components/Chat/Messages/MultiMessage.tsx
+++ client/src/components/Chat/Messages/MultiMessage.tsx
@@ -1,3 +1,76 @@
+// import { useRecoilState } from 'recoil';
+// import { useEffect, useCallback } from 'react';
+// import type { TMessageProps } from '~/common';
+// // eslint-disable-next-line import/no-cycle
+// import Message from './Message';
+// // eslint-disable-next-line import/no-cycle
+// import MessageParts from './MessageParts';
+// import store from '~/store';
+
+// export default function MultiMessage({
+//   // messageId is used recursively here
+//   messageId,
+//   messagesTree,
+//   currentEditId,
+//   setCurrentEditId,
+// }: TMessageProps) {
+//   const [siblingIdx, setSiblingIdx] = useRecoilState(store.messagesSiblingIdxFamily(messageId));
+
+//   const setSiblingIdxRev = useCallback(
+//     (value: number) => {
+//       setSiblingIdx((messagesTree?.length ?? 0) - value - 1);
+//     },
+//     [messagesTree?.length, setSiblingIdx],
+//   );
+
+//   useEffect(() => {
+//     // reset siblingIdx when the tree changes, mostly when a new message is submitting.
+//     setSiblingIdx(0);
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [messagesTree?.length]);
+
+//   useEffect(() => {
+//     if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
+//       setSiblingIdx(0);
+//     }
+//   }, [siblingIdx, messagesTree?.length, setSiblingIdx]);
+
+//   if (!(messagesTree && messagesTree?.length)) {
+//     return null;
+//   }
+
+//   const message = messagesTree[messagesTree.length - siblingIdx - 1];
+
+//   if (!message) {
+//     return null;
+//   }
+
+//   if (message.content) {
+//     return (
+//       <MessageParts
+//         key={message.messageId}
+//         message={message}
+//         currentEditId={currentEditId}
+//         setCurrentEditId={setCurrentEditId}
+//         siblingIdx={messagesTree.length - siblingIdx - 1}
+//         siblingCount={messagesTree.length}
+//         setSiblingIdx={setSiblingIdxRev}
+//       />
+//     );
+//   }
+
+//   return (
+//     <Message
+//       key={message.messageId}
+//       message={message}
+//       currentEditId={currentEditId}
+//       setCurrentEditId={setCurrentEditId}
+//       siblingIdx={messagesTree.length - siblingIdx - 1}
+//       siblingCount={messagesTree.length}
+//       setSiblingIdx={setSiblingIdxRev}
+//     />
+//   );
+// }
 import { useRecoilState } from 'recoil';
 import { useEffect, useCallback } from 'react';
 import type { TMessageProps } from '~/common';
@@ -8,7 +81,6 @@ import MessageParts from './MessageParts';
 import store from '~/store';
 
 export default function MultiMessage({
-  // messageId is used recursively here
   messageId,
   messagesTree,
   currentEditId,
@@ -24,28 +96,39 @@ export default function MultiMessage({
   );
 
   useEffect(() => {
-    // reset siblingIdx when the tree changes, mostly when a new message is submitting.
+    console.log('MessagesTree changed:', messagesTree);
+    console.log('Current messageId:', messageId);
+    console.log('Current siblingIdx:', siblingIdx);
     setSiblingIdx(0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [messagesTree?.length]);
 
   useEffect(() => {
     if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
+      console.log('Resetting siblingIdx to 0');
       setSiblingIdx(0);
     }
   }, [siblingIdx, messagesTree?.length, setSiblingIdx]);
 
   if (!(messagesTree && messagesTree?.length)) {
+    console.log('No messagesTree or empty messagesTree');
     return null;
   }
+
+  console.log('All siblings:', messagesTree);
+  console.log('Current siblingIdx:', siblingIdx);
+  console.log('Total siblings:', messagesTree.length);
 
   const message = messagesTree[messagesTree.length - siblingIdx - 1];
 
   if (!message) {
+    console.log('No message found for current siblingIdx');
     return null;
   }
 
+  console.log('Current message:', message);
+
   if (message.content) {
+    console.log('Rendering MessageParts');
     return (
       <MessageParts
         key={message.messageId}
@@ -59,6 +142,7 @@ export default function MultiMessage({
     );
   }
 
+  console.log('Rendering Message');
   return (
     <Message
       key={message.messageId}
