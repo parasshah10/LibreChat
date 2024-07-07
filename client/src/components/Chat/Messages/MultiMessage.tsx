4fd6c721ad84025b7d2321b60f0492b4cc039d36
parasshah10 <116607050+parasshah10@users.noreply.github.com>
2024-07-07 12:49:14 +0530

    Update MultiMessage.tsx

--- client/src/components/Chat/Messages/MultiMessage.tsx
+++ client/src/components/Chat/Messages/MultiMessage.tsx
@@ -1,77 +1,3 @@
-// import { useRecoilState } from 'recoil';
-// import { useEffect, useCallback } from 'react';
-// import type { TMessageProps } from '~/common';
-// // eslint-disable-next-line import/no-cycle
-// import Message from './Message';
-// // eslint-disable-next-line import/no-cycle
-// import MessageParts from './MessageParts';
-// import store from '~/store';
-
-// export default function MultiMessage({
-//   messageId,
-//   messagesTree,
-//   currentEditId,
-//   setCurrentEditId,
-// }: TMessageProps) {
-//   const [siblingIdx, setSiblingIdx] = useRecoilState(store.messagesSiblingIdxFamily(messageId));
-//   const setSiblingIdxRev = useCallback(
-//     (value: number) => {
-//       setSiblingIdx((messagesTree?.length ?? 0) - value - 1);
-//     },
-//     [messagesTree?.length, setSiblingIdx],
-//   );
-
-//   useEffect(() => {
-      
-//       console.log('Ran first one!')
-//      if (currentEditId !== -1) { // Check if an edit is ongoing
-//       setSiblingIdx(0); 
-//     }
-//   }, [messagesTree?.length]);
-
-//   useEffect(() => {
-//     if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
-//       console.log('Resetting siblingIdx to 0');
-//       setSiblingIdx(0);
-//     }
-//   }, [siblingIdx, messagesTree?.length, setSiblingIdx]);
-
-//   if (!(messagesTree && messagesTree?.length)) {
-//     return null;
-//   }
-
-
-//   const message = messagesTree[messagesTree.length - siblingIdx - 1];
-
-//   if (!message) {
-//     return null;
-//   }
-//   if (message.content) {
-//     return (
-//       <MessageParts
-//         key={message.messageId}
-//         message={message}
-//         currentEditId={currentEditId}
-//         setCurrentEditId={setCurrentEditId}
-//         siblingIdx={messagesTree.length - siblingIdx - 1}
-//         siblingCount={messagesTree.length}
-//         setSiblingIdx={setSiblingIdxRev}
-//       />
-//     );
-//   }
-
-//   return (
-//     <Message
-//       key={message.messageId}
-//       message={message}
-//       currentEditId={currentEditId}
-//       setCurrentEditId={setCurrentEditId}
-//       siblingIdx={messagesTree.length - siblingIdx - 1}
-//       siblingCount={messagesTree.length}
-//       setSiblingIdx={setSiblingIdxRev}
-//     />
-//   );
-// }
 import { useRecoilState } from 'recoil';
 import { useEffect, useCallback } from 'react';
 import type { TMessageProps } from '~/common';
@@ -97,16 +23,16 @@ export default function MultiMessage({
     [messagesTree?.length, setSiblingIdx],
   );
 
-  useEffect(() => {
-    // reset siblingIdx when the tree changes, mostly when a new message is submitting.
-    console.log('Ran First One')
-    // console.log(siblingIdx)     
-    // console.log(messagesTree.length)
-    // if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
-      // setSiblingIdx(0);
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messagesTree?.length]);
+  // useEffect(() => {
+  //   // reset siblingIdx when the tree changes, mostly when a new message is submitting.
+  //   console.log('Ran First One')
+  //   // console.log(siblingIdx)     
+  //   // console.log(messagesTree.length)
+  //   // if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
+  //     // setSiblingIdx(0);
+  //   // }
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [messagesTree?.length]);
 
   useEffect(() => {
     if (messagesTree?.length && siblingIdx >= messagesTree?.length) {
